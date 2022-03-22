872c13e0f6444cbbecbe819628e184d7d9988e7c
msyxxctc5 <50387725+msyxxctc5@users.noreply.github.com>
2022-03-22 15:42:02 +0800

    main

--- main.cpp
+++ main.cpp
@@ -9,37 +9,41 @@ using namespace std;
 
 int main() {
 
-   /* Shape *s1 = new Rectangle("R1",5,6);
+    Shape *s1 = new Rectangle("R1",5,6);
     cout << s1->get_name() << endl;
     cout << s1->get_area() << endl;
     cout <<s1->get_perimeter() << endl;
-    */
 
-   /*Shape *s2 = new Circle("C1",10);
+   Shape *s2 = new Circle("C1",10);
    cout << s2->get_name() << endl;
    cout << s2->get_area() << endl;
    cout <<s2->get_perimeter() << endl;
 
-*/
-   /*Shape * s3 = new Triangle("T1",10,10,10,20);
+   Shape * s3 = new Triangle("T1",10,10,10,20);
    cout << s3->get_name() << endl;
    cout << s3->get_area() << endl;
-   cout <<s3->get_perimeter() << endl;*/
+   cout <<s3->get_perimeter() << endl;
 
    vector<Shape*> shapes;
-   Shape * s1 = new Triangle("T1",10,10,10,20);
-   Shape *s2 = new Circle("C1",10);
-   Shape * s3 = new Triangle("T1",10,10,10,20);
+   /*Shape * s1 = new Triangle("Triangle",10,10,10,20);
+   Shape *s2 = new Circle("Circle",10);
+   Shape * s3  = new Rectangle("Rectangle",5,6);*/
    shapes.push_back(s1);
    shapes.push_back(s2);
    shapes.push_back(s3);
 
-    for (auto i:shapes) {
-        
+   for(auto& i : shapes) {
+       cout << "Shape name is: ";
+       cout << i->get_name();
+       cout << '\n';
+       cout << "Shape area is: ";
+       cout << i->get_area();
+       cout << '\n';
+       cout << "Shape perimeter is: ";
+       cout << i->get_perimeter();
+       cout << endl;
 
-    };
-
-
+   }
 
 
 
